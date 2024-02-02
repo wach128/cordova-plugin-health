cfce377a02620376c4f22bcfb49b6876064e5e69
BenLaKnet <100009654+BenLaKnet@users.noreply.github.com>
2024-02-02 04:57:26 +0100

    Update HealthPlugin.java - delete args

--- src/android/HealthPlugin.java
+++ src/android/HealthPlugin.java
@@ -1252,7 +1252,7 @@ public class HealthPlugin extends CordovaPlugin {
 
                 callbackContext.success();
             } else {
-                if (args.getJSONObject(0).has("startDate")) {
+                if (!args.getJSONObject(0).has("startDate")) {
                     callbackContext.error("Missing argument startDate");
                     return;
                 }
